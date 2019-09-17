12f5628bafbcb3227fd568e97ea4986cd8163b42
Wander Douglas <wanderdasouza@gmail.com>
2019-09-17 14:13:41 -0300

    Add 'liberageral' e 'protegepracaramba' commands

--- EP1/mac0422shell.c
+++ EP1/mac0422shell.c
@@ -1,7 +1,73 @@
-#include<>
+#include <signal.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
+
+ char **parser(char *input) {
+    char **command;
+    char *tokenized;
+    int i;
+
+    command = malloc(32 * sizeof(char *));
+
+    tokenized = strtok(input, " ");
+    i = 0;
+
+    while(tokenized != NULL) {
+        command[i] = tokenized;
+        i = i + 1;
+        tokenized = strtok(NULL, " ");
+    }
+
+    command[i] = NULL;
+    
+    return command;
+
+}
 
 
 int main() {
+
+    char *input;
+    char **command;
+    int result;
+   
+    
+    while(1) {
+        input = malloc(32 * sizeof(char));
+        scanf("%[^\n]", input);
+
+        command = parser(input);        
+
+        if (strcmp(command[0], "protegepracaramba") == 0) {
+            result = chmod(command[1], 0000);
+        }
+
+        else {
+            if (strcmp(command[0], "liberageral") == 0) {
+                result = chmod(command[1], 0777);
+            }
+
+            else {
+                if (1) {
+
+                }
+
+                else {
+
+                }
+            }
+        }
+
+        free(command);
+        free(input);
+
+        return 0;
+    }
     
     return 0;
 }
