ac01cfe4fbe736d820b3a1e40208bda124201c1e
ThiagoSTeixeira <tteixeira.st@gmail.com>
2019-10-10 17:35:17 -0300

    progresso na tarefa 2

--- EP2/kernel/proc.h
+++ EP2/kernel/proc.h
@@ -80,12 +80,24 @@ struct proc {
  * can be set in table.c. IDLE must have a queue for itself, to prevent low 
  * priority user processes to run round-robin with IDLE. 
  */
-#define NR_SCHED_QUEUES   16	/* MUST equal minimum priority + 1 */
+
+/* ######################################################## */
+
+#define NR_SCHED_QUEUES   17	/* MUST equal minimum priority + 1 */
+
+/* ######################################################## */
+
 #define TASK_Q		   0	/* highest, used for kernel tasks */
 #define MAX_USER_Q  	   0    /* highest priority for user processes */   
 #define USER_Q  	   7    /* default (should correspond to nice 0) */   
 #define MIN_USER_Q	  14	/* minimum priority for user processes */
-#define IDLE_Q		  15    /* lowest, only IDLE process goes here */
+
+/* ######################################################## */
+
+#define BATCH_Q     15    /* Nova fila criada para o EP  */
+#define IDLE_Q		  16    /* lowest, only IDLE process goes here */
+
+/* ######################################################## */
 
 /* Magic process table addresses. */
 #define BEG_PROC_ADDR (&proc[0])
