b25769b695c7b9d586b038f8c34647292df6e513
ThiagoSTeixeira <tteixeira.st@gmail.com>
2019-11-13 19:20:39 -0300

    FUNCIONA PORRA CARALHO EH ISSO GARAI

--- EP3/local/src/memory_map.c
+++ EP3/local/src/memory_map.c
@@ -27,6 +27,8 @@
 #include <minix/type.h>
 #include <minix/const.h>
 
+#include "/usr/src/servers/is/inc.h"
+
 #include "/usr/src/servers/pm/mproc.h"
 #include "/usr/src/kernel/const.h"
 #include "/usr/src/kernel/proc.h"
@@ -48,9 +50,21 @@ int print_memory(struct pm_mem_info *pmi)
 
 	return 1;
 }
-
+void print_table(struct mproc mp[]){
+    int i;
+    struct mproc *mp_atual;
+    for(i=0; i < NR_PROCS; i++){
+        mp_atual = &mp[i];
+        printf("pid: %d\t", mp_atual[i].mp_pid);
+        printf("primeira posicao: %d\t", mp_atual[i].mp_seg[T].mem_phys);
+        printf("ultima posicao: %d\n", mp_atual[i].mp_seg[S].mem_phys + mp_atual[i].mp_seg[S].mem_len);
+    }
+}
 int main(int argc, char *argv[]){
     static struct pm_mem_info pmi;
+    static struct mproc mp[NR_PROCS];
+    getsysinfo(PM_PROC_NR, SI_PROC_TAB, mp);
     getsysinfo(PM_PROC_NR, SI_MEM_ALLOC, &pmi);
+    print_table(mp);
     print_memory(&pmi);
 }
